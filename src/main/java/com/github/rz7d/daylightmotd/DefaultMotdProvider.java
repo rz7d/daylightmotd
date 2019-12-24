a7de0dab7612fc4807de03c15cd694ff5f05bbc6
azure <rz7@outlook.jp>
2019-12-24 09:02:41 +0900

    Generalize constructor of DefaultMotdProvider

--- src/main/java/com/github/rz7d/daylightmotd/DefaultMotdProvider.java
+++ src/main/java/com/github/rz7d/daylightmotd/DefaultMotdProvider.java
@@ -10,15 +10,25 @@ import java.util.function.LongPredicate;
 
 public final class DefaultMotdProvider implements MotdProvider {
 
-    private static final Map<LongPredicate, ChatColor> COLOR_MAP = ImmutableMap.of(
+    private static final Map<LongPredicate, ChatColor> DEFAULT_COLOR_MAP = ImmutableMap.of(
         TickDuration.between(0, 12517)::test, ChatColor.WHITE,
         TickDuration.between(12517, 13183)::test, ChatColor.DARK_BLUE,
         TickDuration.between(13183, 22917)::test, ChatColor.DARK_GRAY,
         TickDuration.between(22917, 24000)::test, ChatColor.WHITE
     );
 
+    private final Map<LongPredicate, ChatColor> colorMap;
+
+    public DefaultMotdProvider() {
+        this(DEFAULT_COLOR_MAP);
+    }
+
+    public DefaultMotdProvider(Map<LongPredicate, ChatColor> colorMap) {
+        this.colorMap = colorMap;
+    }
+
     Optional<ChatColor> timeToColor(long time) {
-        return COLOR_MAP.entrySet().stream()
+        return colorMap.entrySet().stream()
             .filter(e -> e.getKey().test(time))
             .map(Map.Entry::getValue)
             .findAny();
